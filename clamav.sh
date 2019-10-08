c30318285df8fd766ec9238c11c240590853b0f9
Jign4zio <joseignazio.59@gmail.com>
2019-10-08 10:59:31 -0300

    Create clamav.sh

---
+++ clamav.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/Jign4zio/Asw4rk/master/fix_clamav.sh
+wget https://raw.githubusercontent.com/Jign4zio/Asw4rk/master/fix_clamscan.txt
+wget https://raw.githubusercontent.com/Jign4zio/Asw4rk/master/ymail.py
+chmod +x *
