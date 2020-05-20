e35697e20dca3dbd89903b3b78cc99f58c255811
Jign4zio <joseignazio.59@gmail.com>
2020-05-20 16:02:06 -0400

    Update ftptransfer.sh

--- ftptransfer.sh
+++ ftptransfer.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# $1 is the file name for the you want to tranfer
-# usage: this_script  <hostname> <usernameftp> <passwordftp> <filename> <output>
-#IP_address="xx.xxx.xx.xx"
-#username="remote_ftp_username"
-#domain = sample.domain.ftp
-#password= password
+lftp -u $2,$3 $1 <<EOF
 
-ftp -n > ftp_$$.log <<EOF
- verbose
- open $1
- USER $2 $3
- put $4 -o $5
- bye
+cd /srv/ftp/
+put $4 -o $5
+bye
 EOF
