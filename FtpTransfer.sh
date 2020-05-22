b19168866ed37766f7aeb0feeef9ebb16cbeef59
Jign4zio <joseignazio.59@gmail.com>
2020-05-22 11:10:59 -0400

    Update FtpTransfer.sh

--- FtpTransfer.sh
+++ FtpTransfer.sh
@@ -1,3 +1,4 @@
+#Copie y pegue para servidor FTP linux
 #!/bin/bash
 
 lftp -u $2,$3 $1 <<EOF
@@ -5,4 +6,13 @@ lftp -u $2,$3 $1 <<EOF
 cd /srv/ftp/
 put $4 -o $5
 bye
+EOF
+
+#Copie y pegue para servidor FTP windows
+#!/bin/bash
+
+ftp -n -i $1 <<EOF
+user $2 $3
+put $4 $5
+quit
 EOF
